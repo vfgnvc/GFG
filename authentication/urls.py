5609b72d9f42f8c11aec6e515523bee062416c5a
Abishek Anto <abishekantojesto@gmail.com>
2022-02-01 18:05:09 +0530

    initial commit

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from os import name
+from xml.etree.ElementInclude import include
+from django import views
+from django.contrib import admin
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+    path('',views.home,name="home"),
+]
